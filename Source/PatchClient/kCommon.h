51f8c01dcdeea2d86ba4de20507217e86a57b578
zOmbie <29067524+NukeZero@users.noreply.github.com>
2024-06-20 13:23:24 -0400

    Initial commit
    This is first commit for the original base of my L19.4.

---
+++ Source/PatchClient/kCommon.h
@@ -0,0 +1,11 @@
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+#define	NEUZ_TITLE		"Your server name"
+#define	NEUZ_BHASH		"sunkist" // Must match your neuz.
+#define WEB_FIRST_PAGE	"http://mycoolflyff.com/index.html"
+#define SERVER_ROOT		"/RESCLIENT/"
+#define PATCHSEVER_URL	"patcher.mycoolflyff.com"
+#define HOME_LINK		"http://mycoolflyff.com/"
+#define REGISTER_LINK	"http://mycoolflyff.com/register.html"
+
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
---
+++ Source/_Common/LodeConfig.h
@@ -0,0 +1,27 @@
+#pragma once
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+/*
+This file came from https://lodelight.com
+ -> For support and other releases be sure to visit us today.
+*/
+
+#define	NEUZ_TITLE	"[VS22] Lodelight V19.4"			//: Window title of the client.
+#define	NEUZ_BHASH	"sunkist"							//: String to boot the client.
+#define	NEUZ_IPSET	"127.0.0.1"							//: Ip of the server, must match AccountServer.ini & LoginServer.ini.
+#define	NEUZ_CPORT	"5400"								//: Cache port, must match CacheServer.ini.
+#define	NEUZ_PHASH	"kikugalanet"						//: Md5 password salt.
+#define	NEUZ_MSGVR	"20100412"							//: Ver of the server, must match AccountServer.ini & LoginServer.ini.
+
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+#define	__FIX_STAT_BUFF									//: Fixes some issues present with the v19 spec_item.
+#define	__KIA_GOALDATA_FIX								//: Fixes quest arrow bug.
+#define __SLEEPING_FIX									//: Fixes the stun/sleeping bug present with Echoes of Weary.
+#define __SYS_SCR_CANCELLATION							//: Scroll of Unbinding.
+#define	__NAVIGATOR_V19									//: V19 Navigator.
+#define __NEW_EXCHANGE_V19								//: New exchange windows with arrow icons.
+#define	__NEW_TASKBAR_V19								//: V19 Taskbar.
+#define __TREASUREBOX									//: Retail like treasure box system.
+#define	__STATISTICS									//: Extra stats in the char window.
+
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
